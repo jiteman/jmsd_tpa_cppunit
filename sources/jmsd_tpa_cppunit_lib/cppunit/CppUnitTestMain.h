308584006f6579b0c251edec78eec30bafa9e3ee
jiteman <jiteman@gmail.com>
2019-11-18 15:21:07 +0300

    jmsd_tpa_cppunit_lib has been added

---
+++ sources/jmsd_tpa_cppunit_lib/cppunit/CppUnitTestMain.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+#include "tpa_cppunit_lib.h"
+
+
+int JMSD_TPA_CPPUNIT_LIBRARY_SHARED_INTERFACE test_cppunit_main( int argc, char const *const argv[] );
--- sources/jmsd_tpa_cppunit_test/cppunit/CppUnitTestMain.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-
-#include "cppunit_test.h"
-
-
-int JMSD_TPA_CPPUNIT_TEST_SHARED_INTERFACE test_cppunit_main( int const argc, char const *const argv[] );
